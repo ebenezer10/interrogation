a81ed4e6e83ac9fc4166f0e5aebde68cd12e7f6a
Ebenezer Nikabou <nikaboue10@gmail.com>
2021-08-19 00:19:49 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function getAnswer(question) {
+  return new Promise((success, error) => {
+    rl.question(question, (answer) => {
+      success(answer);
+    }, () => {
+      error();
+    });
+  });
+}
+
+let response;
+const responseArray = [];
+async function mainFunc(questions) {
+  // eslint-disable-next-line no-restricted-syntax
+  for await (const item of questions) {
+    response = await getAnswer(item);
+    responseArray.push({ question: item, response });
+  }
+  rl.close();
+  return responseArray;
+}
+
+const questioner = async function (questions) {
+  const res = await mainFunc(questions);
+  return res;
+};
+
+export default questioner;
